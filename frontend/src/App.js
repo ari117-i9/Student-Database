53e221f47167816a3a15ddd2076eeecb45cf31cc
ari <ari.imran.f@gmail.com>
2026-06-26 18:55:40 +0800

    minor test for student form and add student

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Nav, Navbar, Container, Row, Col }
+    from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import {
+    BrowserRouter as Router, Routes,
+    Route, Link
+} from "react-router-dom";
+import AddStudent from
+    "./Components/add-student.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <div className="App">
+                <header className="App-header">
+                    <Navbar bg="dark" variant="dark">
+                        <Container>
+                            <Navbar.Brand>
+                                <Link to={"/add-student"}
+                                    className="nav-link">
+                                    React MERN Stack App
+                                </Link>
+                            </Navbar.Brand>
+                            <Nav className="justify-content-end">
+                                <Nav>
+                                    <Link to={"/add-student"}
+                                        className="nav-link">
+                                        Add Student
+                                    </Link>
+                                </Nav>
+                            </Nav>           
+                        </Container>
+                    </Navbar>
+                 </header>
+
+                 <Container>
+                    <Row>
+                        <Col md={12}>
+                            <div className="wrapper">
+                                <Routes>
+                                    <Route path="/" element={<AddStudent />} />
+                                    <Route path="/add-student" element={<AddStudent />} />
+                                </Routes>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+    </Router>             
+
+
   );
 }
 
