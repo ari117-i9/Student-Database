de5dc47bd8fa430822cfc3ea91e5b7b72b91e0a5
ari <ari.imran.f@gmail.com>
2026-06-28 22:15:37 +0800

    minor test 2 for student form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,49 +10,62 @@ import {
 } from "react-router-dom";
 import AddStudent from
     "./Components/add-student.component";
+import EditStudent from
+    "./Components/edit-student.component";
+import StudentList from
+    "./Components/student-list.component";
 
-function App() {
-  return (
-    <Router>
+// App Component
+const App = () => {
+    return (
+        <Router>
             <div className="App">
                 <header className="App-header">
                     <Navbar bg="dark" variant="dark">
                         <Container>
                             <Navbar.Brand>
-                                <Link to={"/add-student"}
+                                <Link to={"/create-student"}
                                     className="nav-link">
                                     React MERN Stack App
                                 </Link>
                             </Navbar.Brand>
+
                             <Nav className="justify-content-end">
                                 <Nav>
-                                    <Link to={"/add-student"}
+                                    <Link to={"/create-student"}
                                         className="nav-link">
-                                        Add Student
+                                        Create Student
                                     </Link>
                                 </Nav>
-                            </Nav>           
+
+                                <Nav>
+                                    <Link to={"/student-list"}
+                                        className="nav-link">
+                                        Student List
+                                    </Link>
+                                </Nav>
+                            </Nav>
                         </Container>
                     </Navbar>
-                 </header>
+                </header>
 
-                 <Container>
+                <Container>
                     <Row>
                         <Col md={12}>
                             <div className="wrapper">
                                 <Routes>
-                                    <Route path="/" element={<AddStudent />} />
-                                    <Route path="/add-student" element={<AddStudent />} />
+                                    <Route path="/" element={<CreateStudent />} />
+                                    <Route path="/create-student" element={<CreateStudent />} />
+                                    <Route path="/edit-student/:id" element={<EditStudent />} />
+                                    <Route path="/student-list" element={<StudentList />} />
                                 </Routes>
                             </div>
                         </Col>
                     </Row>
                 </Container>
             </div>
-    </Router>             
-
-
-  );
-}
+        </Router>
+    );
+};
 
 export default App;
